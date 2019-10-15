885eceddc3b42fd41a63b258c31d232d2741c6db
Aidan Oakley <oakleyaidan21@github.com>
2019-10-15 16:59:00 -0400

    started links page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,7 @@ import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
-import { MonoText } from "../components/StyledText";
 import KalimbaContainer from "../components/KalimbaComponents/KalimbaContainer.js";
-import SelectorContainer from "../components/KalimbaComponents/SelectorContainer";
-// import KalimbaContainer from "../components/KalimbaComponents/KalimbaContainer.jsx";
 
 export default function HomeScreen() {
   return (
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { View, Text } from "react-native";
+import { ExpoLinksView } from "@expo/samples";
+import HyperLink from "react-native-hyperlink";
 
 export default function LinksScreen() {
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
+    <View style={{ padding: 10, alignItems: "center" }}>
+      <Text style={{ fontSize: 30 }}>Link to Kalimba Libre:</Text>
+      <Text style={{ fontSize: 30 }}>Github Repos: </Text>
+      <HyperLink
+        linkStyle={{ color: "#2980b9", fontSize: 20 }}
+        linkText={url =>
+          url === "https://github.com/oakleyaidan21/KalimbaLibre" ? "Here" : url
+        }
+      >
+        <Text style={{ fontSize: 15 }}>Kalimba Libre Repo Here</Text>
+      </HyperLink>
+      <HyperLink
+        linkStyle={{ color: "#2980b9", fontSize: 20 }}
+        linkText={url =>
+          url === "https://github.com/oakleyaidan21/KalimBasic" ? "Here" : url
+        }
+      >
+        <Text style={{ fontSize: 15 }}>KalimBasic Repo Here</Text>
+      </HyperLink>
+    </View>
   );
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  title: "About KalimBasic"
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
