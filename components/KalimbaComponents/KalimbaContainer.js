8b2317c064ee2074dc92481cbadf7fd2b5f697ef
Aidan Oakley <oakleyaidan21@github.com>
2019-10-16 11:58:14 -0400

    removed some comments

--- components/KalimbaComponents/KalimbaContainer.js
+++ components/KalimbaComponents/KalimbaContainer.js
@@ -36,30 +36,6 @@ class KalimbaContainer extends Component {
           top: 0
         }}
       >
-        {/* {this.global.tines.map((tine, index) => (
-          <TouchableOpacity
-            key={"tine # " + index}
-            style={{
-              backgroundColor: (tine.color),
-              width: 22,
-              height: 500 + tine.len * 6,
-              position: "absolute",
-              left: 24.5 * index,
-              float: "left",
-              borderRadius: 10,
-              top: topMarg,
-              alignItems: "center",
-              fontSize: 20
-            }}
-            onPress={() => {
-              this.handleTineTap(tine);
-            }}
-          >
-            <Text style={{ top: 470 + tine.len * 6, fontSize: 10 }}>
-              {tine.note}
-            </Text>
-          </TouchableOpacity>
-        ))} */}
         {this.global.tines.map((tine, index) => {
           var color = "white";
           if ((index + 1) % 3 === 0) {
